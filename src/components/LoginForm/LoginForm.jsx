9595a305197c48022542ca0d2605b45028900735
dfung003 <dfung003@ucr.edu>
2022-04-23 14:17:57 -0700

    components added

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,26 +1,29 @@
 import { useState } from 'react';
-import * as userService from '../../utilities/users-service';
-
+import * as usersService from '../../utilities/users-service';
 
 export default function LoginForm({ setUser }) {
     const [credentials, setCredentials] = useState({
         email: '',
         password: ''
-    })
-    const [error, setError] = useState('')
+    });
+    const [error, setError] = useState('');
 
-    const handleChange = (evt) => {
+    function handleChange(evt) {
         setCredentials({ ...credentials, [evt.target.name]: evt.target.value });
         setError('');
     }
 
-    const handleSubmit = async (evt) => {
-        evt.preventDefault()
+    async function handleSubmit(evt) {
+        // Prevent form from being submitted to the server
+        evt.preventDefault();
         try {
-            const user = await userService.login(credentials);
-            setUser(user)
-        } catch (error) {
-            setError(error.message)
+            // The promise returned by the signUp service method
+            // will resolve to the user object included in the
+            // payload of the JSON Web Token (JWT)
+            const user = await usersService.login(credentials);
+            setUser(user);
+        } catch {
+            setError('Log In Failed - Try Again');
         }
     }
 
@@ -29,14 +32,13 @@ export default function LoginForm({ setUser }) {
             <div className="form-container">
                 <form autoComplete="off" onSubmit={handleSubmit}>
                     <label>Email</label>
-                    <input type="email" name="email" value={credentials.email} onChange={handleChange} required />
+                    <input type="text" name="email" value={credentials.email} onChange={handleChange} required />
                     <label>Password</label>
                     <input type="password" name="password" value={credentials.password} onChange={handleChange} required />
                     <button type="submit">LOG IN</button>
                 </form>
             </div>
-            <h1 className="error-message">&nbsp;{error}</h1>
+            <p className="error-message">&nbsp;{error}</p>
         </div>
-    )
-
+    );
 }
--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { Component } from "react";
 import { signUp } from '../../utilities/users-service';
 
 export default class SignUpForm extends Component {
@@ -8,26 +8,36 @@ export default class SignUpForm extends Component {
         password: '',
         confirm: '',
         error: ''
-    }
+    };
 
     handleChange = (evt) => {
-        this.setState({ ...this.state, [evt.target.name]: evt.target.value, error: '' })
-    }
+        this.setState({
+            [evt.target.name]: evt.target.value,
+            error: ''
+        });
+    };
 
     handleSubmit = async (evt) => {
         evt.preventDefault();
         try {
             const formData = { ...this.state };
-            delete formData.error;
             delete formData.confirm;
-            const user = await signUp(formData)
-            this.props.setUser(user)
-        } catch (error) {
-            this.setState({ error: 'Sign Up Failed' })
+            delete formData.error;
+            // The promise returned by the signUp service method
+            // will resolve to the user object included in the
+            // payload of the JSON Web Token (JWT)
+            const user = await signUp(formData);
+            // Baby step
+            this.props.setUser(user);
+        } catch {
+            // An error happened on the server
+            this.setState({ error: 'Sign Up Failed - Try Again' });
         }
-    }
+    };
 
-
+    // We must override the render method
+    // The render method is the equivalent to a function-based component
+    // (its job is to return the UI)
     render() {
         const disable = this.state.password !== this.state.confirm;
         return (
@@ -45,7 +55,7 @@ export default class SignUpForm extends Component {
                         <button type="submit" disabled={disable}>SIGN UP</button>
                     </form>
                 </div>
-                <h1 className="error-message">&nbsp;{this.state.error}</h1>
+                <p className="error-message">&nbsp;{this.state.error}</p>
             </div>
         );
     }
