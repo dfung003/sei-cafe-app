32b85f5a4413289344c7544b818db95c473406cf
dfung003 <dfung003@ucr.edu>
2022-04-23 14:02:14 -0700

    pages set up

--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './pages/App/App';
-// Import the top-level BrowserRouter component
-// gives access to state and ref hooks (look at DevTools)
 import { BrowserRouter as Router } from 'react-router-dom';
 
 
-const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </StrictMode>,
-  rootElement
+  <React.StrictMode>
+    <Router><App /></Router>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
