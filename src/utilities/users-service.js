cebb43572220637a56df092e22f204e625f80545
dfung003 <dfung003@ucr.edu>
2022-04-23 14:24:35 -0700

    added utilities

--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -1,55 +1,41 @@
 import * as usersAPI from './users-api';
 
-
-//Sign Up
 export async function signUp(userData) {
+    // Delete the network request code to the
+    // users-api.js module which will ultimately
+    // return the JWT
     const token = await usersAPI.signUp(userData);
-    localStorage.setItem('token', token);
-    return token;
-}
-
-// Login
-
-export async function login(credentials) {
-    const token = await usersAPI.login(credentials);
+    // Persist the token to localStorage
     localStorage.setItem('token', token);
     return getUser();
 }
 
-
-// Get Token
+export async function login(credentials) {
+    const token = await usersAPI.login(credentials);
+    // Persist the token to localStorage
+    localStorage.setItem('token', token);
+    return getUser();
+}
 
 export function getToken() {
     const token = localStorage.getItem('token');
+    // getItem will return null if no key
     if (!token) return null;
-
-    const payload = JSON.parse(window.atob(token.split('.')[1]))
+    const payload = JSON.parse(atob(token.split('.')[1]));
+    // A JWT's expiration is expressed in seconds, not miliseconds
     if (payload.exp < Date.now() / 1000) {
+        // Token has expired
         localStorage.removeItem('token');
         return null;
     }
-
     return token;
 }
 
-
-// GetUser
-
 export function getUser() {
     const token = getToken();
-    return token ? JSON.parse(window.atob(token.split('.')[1])).user : null;
+    return token ? JSON.parse(atob(token.split('.')[1])).user : null;
 }
 
-
-// logout
-
-export function logout() {
-    localStorage.removeItem('token')
-}
-
-// checkToken
-
-export function checkToken() {
-    return usersAPI.checkToken()
-        .then(dateStr => new Date(dateStr))
+export function logOut() {
+    localStorage.removeItem('token');
 }
